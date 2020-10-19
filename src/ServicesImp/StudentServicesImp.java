31d978a5f4860f522a169a4a0e4f62ff1ed7c7ad
mohamed085 <mohamed085@gmail.com>
2020-10-19 11:56:47 +0200

    Insert Student

--- src/ServicesImp/StudentServicesImp.java
+++ src/ServicesImp/StudentServicesImp.java
@@ -1,6 +1,16 @@
 package ServicesImp;
 
+import DAO.StudentDAO;
+import Model.Student;
 import Services.StudentServices;
 
 public class StudentServicesImp implements StudentServices {
+    StudentDAO studentDAO;
+
+    @Override
+    public void addStudent(Student student) {
+        studentDAO = new StudentDAO();
+        studentDAO.insertStudent(student);
+    }
+
 }
