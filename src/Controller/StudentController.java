5e7168d96f739fadaaec0b1fa72ee98e289b11f4
mohamed085 <mohamed085@gmail.com>
2020-10-19 12:05:32 +0200

    Update Student

--- src/Controller/StudentController.java
+++ src/Controller/StudentController.java
@@ -1,9 +1,73 @@
 package Controller;
 
+import Model.Student;
+import Services.StudentServices;
+import ServicesImp.StudentServicesImp;
+
 import java.util.Scanner;
 
 public class StudentController {
+    StudentServices studentServices;
+    Student student;
+    Scanner input=new Scanner(System.in);
 
+
+    public void updateStudent(){
+        student = new Student();
+        String username;
+        studentServices=new StudentServicesImp();
+        System.out.print("Add username : ");
+        username=input.nextLine();
+        student.setUsername(username);
+        System.out.print("Add password : ");
+        student.setPassword(input.nextLine());
+        System.out.print("Add name : ");
+        student.setName(input.nextLine());
+        System.out.print("Add mail : ");
+        student.setMail(input.nextLine());
+        System.out.print("Add phone : ");
+        student.setPhone(input.nextLine());
+        System.out.print("Add balance : ");
+        student.setBalance(input.nextDouble());
+        System.out.print("Add registerDate : ");
+        studentServices.updateStudent(username,student);
+    }
+    
+    public void updateStudentName(){
+        studentServices=new StudentServicesImp();
+        System.out.print("Enter username : ");
+        String username=input.nextLine();
+        System.out.print("Enter new name : ");
+        String newName=input.nextLine();
+        studentServices.updateStudentName(username,newName);
+    }
+
+    public void updateStudentPassword(){
+        studentServices=new StudentServicesImp();
+        System.out.print("Enter username : ");
+        String username=input.nextLine();
+        System.out.print("Enter new password : ");
+        String newPassword=input.nextLine();
+        studentServices.updateStudentPassword(username,newPassword);
+    }
+
+    public void updateStudentMail(){
+        studentServices=new StudentServicesImp();
+        System.out.print("Enter username : ");
+        String username=input.nextLine();
+        System.out.print("Enter new mail : ");
+        String newMail=input.nextLine();
+        studentServices.updateStudentMail(username,newMail);
+    }
+
+    public void updateStudentPhone(){
+        studentServices=new StudentServicesImp();
+        System.out.print("Enter username : ");
+        String username=input.nextLine();
+        System.out.print("Enter new phone : ");
+        String newPhone=input.nextLine();
+        studentServices.updateStudentPhone(username,newPhone);
+    }
 
 
 
@@ -16,6 +80,13 @@ public class StudentController {
             while (c != 0) {
 
 
+                System.out.print("Please Enter 2 to update student\n");
+                System.out.print("Please Enter 3 to update student name\n");
+                System.out.print("Please Enter 4 to update student password\n");
+                System.out.print("Please Enter 5 to update student mail\n");
+                System.out.print("Please Enter 6 to update student phone\n");
+
+
 
                 System.out.println("Please Enter 0 to exist\n");
                 System.out.print("Please Enter choice : ");
@@ -25,6 +96,21 @@ public class StudentController {
                     case 0:
                         System.exit(0);
 
+                    case 2:
+                        studentController.updateStudent();
+                        break;
+                    case 3:
+                        studentController.updateStudentName();
+                        break;
+                    case 4:
+                        studentController.updateStudentPassword();
+                        break;
+                    case 5:
+                        studentController.updateStudentMail();
+                        break;
+                    case 6:
+                        studentController.updateStudentPhone();
+                        break;
 
 
                     default:
--- src/ServicesImp/StudentServicesImp.java
+++ src/ServicesImp/StudentServicesImp.java
@@ -1,6 +1,41 @@
 package ServicesImp;
 
+import DAO.StudentDAO;
+import Model.Student;
 import Services.StudentServices;
 
 public class StudentServicesImp implements StudentServices {
+
+    StudentDAO studentDAO;
+
+    @Override
+    public void updateStudent(String username, Student newStudent) {
+        studentDAO = new StudentDAO();
+        studentDAO.updateStudent(username, newStudent);
+    }
+
+    @Override
+    public void updateStudentName(String username, String newName) {
+        studentDAO = new StudentDAO();
+        studentDAO.updateStudentName(username, newName);
+    }
+
+    @Override
+    public void updateStudentPassword(String username, String newPassword) {
+        studentDAO = new StudentDAO();
+        studentDAO.updateStudentPassword(username, newPassword);
+    }
+
+    @Override
+    public void updateStudentMail(String username, String newMail) {
+        studentDAO = new StudentDAO();
+        studentDAO.updateStudentMail(username, newMail);
+    }
+
+    @Override
+    public void updateStudentPhone(String username, String newPhone) {
+        studentDAO = new StudentDAO();
+        studentDAO.updateStudentPhone(username, newPhone);
+    }
+
 }
